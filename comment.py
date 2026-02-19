6f585e357755c5f32e2ce844a193fb5442f8062e
Atharva Babhane <atharvastudy100@gmail.com>
2026-02-19 20:27:48 +0530

    Fix formatting and add commented code block

---
+++ comment.py
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//This program prints Hello World
+int main() 
+{
+printf("Hello World");
+return 0;
+}
+
+#include<stdio.h>
+(* 
+int main() 
+{
+printf("Hello World");
+return 0;
+} *)
